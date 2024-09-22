1428c0b4ee111f76a57ea6f4e34e44c27da36c66
igor-Kirov <konstantinov959@gmail.com>
2024-09-22 19:18:23 +0300

    Add files via upload
    Практическое задание по уроку "Базовые структуры данных"

---
+++ practical_assignment.py
@@ -0,0 +1,16 @@
+practical assi
+#1st program
+print(9**0.5*5)
+
+#2nd program
+print((9.99 > 9.98) and (1000 != 1000.1))
+
+#3rd program
+print(2*2+2)
+print(2*(2+2))
+print((2*2+2) == (2*(2+2)))
+
+#4th program
+i=float('123.456')
+i=int(i*10)%10
+print(i)gnment
