4d555a21cd738c277ceb43234b29ea2979286a4f
igor-Kirov <konstantinov959@gmail.com>
2024-09-24 20:27:35 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,9 @@
+name = "Igor"
+print('Name: '+name)
+age = 60
+print(f'Age: {age}')
+new_age=10
+age=age+new_age
+print(f'New Age: {age}')
+is_student = True
+print(f'Is_Student: {is_student}')
---
+++ module_1_4.py
@@ -0,0 +1,8 @@
+my_string =input("Введите текст: ")
+print("В веденной строке - ", len(my_string), "символов")
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ', ''))
+print(my_string[0])
+print(my_string[-1])
+
