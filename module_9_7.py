bd7dd15fa97af18724f0d1b661057a3b74ce78d5
Игорь <konstantinov959@gmail.com>
2024-12-07 16:03:37 +0300

    Add files via upload

---
+++ module_9_7.py
@@ -0,0 +1,21 @@
+
+def is_prime(func):
+    def do_func(*args, **kwargs):
+        k = 0
+        n = func(*args, **kwargs)
+        for j in range(2, n):
+           if n % j == 0:
+                k = k + 1
+        if k == 0:
+            print('Простое')
+        return n
+    return do_func
+
+
+@ is_prime
+def sum_three(*agrs):
+    return sum(agrs)
+
+
+result = sum_three(2, 3, 6)
+print(result)
